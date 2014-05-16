d187c1a53f1e7b62c2ae3cd7ca8b405a32596d26
alxandre <Alx_andre1993@hotmail.com>
2014-05-16 16:48:39 -0400

    binding et typo fix

--- vue.py
+++ vue.py
@@ -1,8 +1,11 @@
+from tkinter import *
+
 class Vue():
     def __init__(self,parent):
         self.parent = parent
         self.root = Tk()
-        self.canevas = Canvas(self.root,225,225)
+        self.canevas = Canvas(self.root,width=225,height=225)
+        self.click = False
 
     def afficherEtatJeu(self,carre,pions,bordures):
         self.canevas.delete("carre")
@@ -16,10 +19,22 @@ class Vue():
         for i in bordures:
             self.canevas.create_rectangle(i.x1,i.y1,i.x+ i.x2,i.y2 ,
                                           fill=i.couleur,tags ="bordures")
+        
 
     def bindMouse(self):
-        self.canevas.bind("<button-1>", self.parent.click)
+        self.canevas.bind("<button-1>", self.click)
+        self.canevas.bind("<B1-Motion>", selft.dragged)
+
+    def click(self,event):
+        self.curseurPosX = event.x
+        self.curseurPosY = event.y
+        
+    def dragged(self,event):
+        self.parent.changementPodCarre(event,self.curseurPosX,self.curseurPosY)
+        self.curseurPosX = event.x
+        self.curseurPosY = event.y
+
 
     def afficherTemps(self,temps):
-        tkMessageBox.showinfo("Score", temps)
+        tkMessageBox.showinfo("Score", temps ,)
     
