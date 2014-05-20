8e5b44fe4d66197966876da73d5564f86c1075c8
wild006 <wild006@hotmail.com>
2014-05-19 22:06:36 -0400

    Ajout changementPos (Pion)

--- modele.py
+++ modele.py
@@ -21,12 +21,16 @@ class Partie():
         self.pions = []
         self.bordures = []
         
-        self.pions.append(Pion(100,100,60,60))
-        self.pions.append(Pion(300,85,60,50))
-        self.pions.append(Pion(85,350,30,60))
-        self.pions.append(Pion(355,340,100,20))
+        vitesseDeBase = 4 #Pour avoir toute la meme vitesse au depart
+        
+        self.pions.append(Pion(self,100,100,60,60, vitesseDeBase, vitesseDeBase))
+        self.pions.append(Pion(self,300,85,60,50, -vitesseDeBase, vitesseDeBase))
+        self.pions.append(Pion(self,85,350,30,60, vitesseDeBase, -vitesseDeBase))
+        self.pions.append(Pion(self,355,340,100,20, -vitesseDeBase, -vitesseDeBase))
         
     def jouer(self):
+        for pion in self.pions:
+            pion.changementPos()
         self.parent.parent.afficherEtatJeu(self.c,self.pions,self.bordures)
         self.parent.parent.v.root.after(50,self.jouer)
         
@@ -54,15 +58,30 @@ class Carre():
                     exit(0)
         
 class Pion():
-    def __init__(self,x,y, largeur, hauteur):
+    def __init__(self,parent,x,y, largeur, hauteur, vitesseX, vitesseY):
+        self.parent = parent
         self.x1 = x #haut/gauche
         self.y1 = y
         self.x2 = x + largeur #bas/droite
         self.y2 = y + hauteur
+        self.vitesseX = vitesseX
+        self.vitesseY = vitesseY
         self.couleur ="blue"
-        
     
-
+    def changementPos(self):
+        self.x1 = self.x1 + self.vitesseX
+        self.x2 = self.x2 + self.vitesseX
+        self.y1 = self.y1 + self.vitesseY
+        self.y2 = self.y2 + self.vitesseY
+        if self.x1 <= 0 and self.vitesseX < 0:
+            self.vitesseX = self.vitesseX * -1
+        if self.x2 >= self.parent.parent.grandeurJeuX and self.vitesseX > 0:
+            self.vitesseX = self.vitesseX * -1
+        if self.y1 <= 0 and self.vitesseY < 0:
+            self.vitesseY = self.vitesseY * -1
+        if self.y2 >= self.parent.parent.grandeurJeuY and self.vitesseY > 0:
+            self.vitesseY = self.vitesseY * -1
+            
 class Bordures():
     def __init__(self,x,y,largeur,hauteur):
         self.x1 =x 
